ea59a09236f74f549e94b2e7544c9f29e269e78f
Syam <syamnarendra85@gmail.com>
2025-07-12 13:31:08 +0530

    tab icon/heading changed

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,14 @@
 import { NavBar } from "~/Components/Navbar";
 import VideoPage from "~/Components/VideoHomePage";
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
 
-const Index = () => {
+export const meta: MetaFunction = () => [{ title: "Pure Plants" }];
+
+export const links: LinksFunction = () => [
+  { rel: "icon", href: "/favicon.png", type: "image/png" },
+];
+
+const Index: React.FC = () => {
   return (
     <div>
       <div className="relative min-h-screen bg-[#f5f3f0]">
