075670b29aba5707fc019332ee64ee3a69d15323
Syam <syamnarendra85@gmail.com>
2025-07-23 11:48:35 +0530

    contact us bg color changed

--- app/routes/contact-us.tsx
+++ app/routes/contact-us.tsx
@@ -5,7 +5,7 @@ import { businessName } from "~/data/text.en";
 export default function Index() {
   return (
     <div className=" bg-black py-12 px-4">
-      <div className="max-w-4xl mx-auto min-h-screen">
+      <div className="max-w-4xl mx-auto h-fit">
         <div className="text-center mb-12">
           <h1 className="text-4xl font-bold text-white mb-4">{businessName}</h1>
           <p className="text-lg text-white">Get in touch with our team</p>
